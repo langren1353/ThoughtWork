2702bdc3b141053325d34c1ffa6a26ea6a9cb3dd
langren1353 <1353464539@qq.com>
2018-05-26 11:45:06 +0800

    更新：采用expect断言语句

--- taxi-fee/main/main.js
+++ taxi-fee/main/main.js
@@ -1,11 +1,18 @@
+var pricePerMile = 0.8;// 每公里0.8元
+var pricePerMile_BIG = 1.2;// 50%溢价
+var upMile = 2; // 两公里
+var toMile = 8; // 8公里后会加收50%的每公里运价
+var startPrice = 6; // 起步价两公里6元
+var stopWaitPrice = 0.25; // 停车等待加收0.25/元
+
 module.exports = function main(miles, stopTime) {
-    var pricePerMile = 0.8;// 每公里0.8元
-    var pricePerMile_BIG = 1.2;// 50%溢价
-    var upMile = 2; // 两公里
-    var toMile = 8; // 8公里后会加收50%的每公里运价
-    var startPrice = 6; // 起步价两公里6元
-    var stopWaitPrice = 0.25; // 停车等待加收0.25/元
     // 开始计算
+    price = parseInt(CountPrice(miles, stopTime));
+    console.log("运行" + miles + "公里,停车等待"+stopTime+"分钟");
+    console.log("花了：" + price + "元");
+    return price;
+};
+function CountPrice(miles, stopTime){
     var price = startPrice;
     if(miles < toMile){
         // 8公里以内
@@ -20,9 +27,6 @@ module.exports = function main(miles, stopTime) {
         // 2公里 + 6公里 + (n-8)公里
         price += 0.8*(toMile-upMile) + (miles-toMile)*pricePerMile_BIG;
     }
+    // 加上停顿的时间
     price += stopWaitPrice*stopTime;
-    price = parseInt(price);
-    console.log("运行" + miles + "公里,停车等待"+stopTime+"分钟");
-    console.log("花了：" + price + "元");
-    return 'Hello World!';
-};
+}
--- taxi-fee/spec/main-spec.js
+++ taxi-fee/spec/main-spec.js
@@ -1,14 +1,35 @@
-const main = require('../main/main');
+"use strict"
+var chai = require("chai");
+var expect = chai.expect;
+var main = require('../main/main');
 
 describe('taxi fee', function () {
-/*
-我们考虑出租车计价问题，出租车的运价是每公里0.8元，八公里起会加收50%的每公里运价，起步价是两公里以内6块，停车等待时加收每分钟0.25元，最后计价的时候司机会四舍五入只收整块钱。
-*/
+    /*
+    我们考虑出租车计价问题，出租车的运价是每公里0.8元，八公里起会加收50%的每公里运价，起步价是两公里以内6块，停车等待时加收每分钟0.25元，最后计价的时候司机会四舍五入只收整块钱。
+    */
     // main(miles, stopTime);
-    main(2, 0);
-    main(5, 0);
-    main(5, 10);
-    main(8, 0);
-    main(9, 0);
-    main(9, 10);
+    it(" // 2公里以内6块", function(){
+        var result = main(2, 0);
+        expect(result).to.equal(6);
+    });
+    it(" // 5公里 = 6 + 3*0.8 = 8.4 ->8块", function(){
+        var result = main(5, 0);
+        expect(result).to.equal(8);
+    });
+    it(" // 5公里+10分钟 = 8.4 + 0.25*10 = 10.9 ->10块", function(){
+        var result = main(5, 10);
+        expect(result).to.equal(10);
+    });
+    it(" // 8公里 = 6 + 6*0.8 = 10块", function(){
+        var result = main(8, 0);
+        expect(result).to.equal(10);
+    });
+    it(" // 9公里 = 6 + 6*0.8 + 1*1.2 = 12块", function(){
+        var result = main(9, 0);
+        expect(result).to.equal(12);
+    });
+    it(" // 9公里+10分钟 = 6 + 6*0.8 + 1*1.2 + = 14块", function(){
+        var result = main(9, 10);
+        expect(result).to.equal(14);
+    });
 });
